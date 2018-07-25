2868697916b0faeaff1059ef7c57de8ed5ef4120
Circler-K <dkdlelgksthf@gmail.com>
2018-07-25 20:11:12 +0900

    fin mbr.py

--- mbr/mbr.py
+++ mbr/mbr.py
@@ -1,14 +1,12 @@
 import struct
 import sys
 
-
 def main():
 	global current_sector, current_sector_size,f, ebr_start_sector
 	filename = "mbr_128.dd"
 	f = open(filename,"rb")
 	current_sector = 0
 	data = read_sectors(f,current_sector)
-
 	if chk_mbr_sign(data) != 1:
 		print("이 파티션은 MBR이 아님")
 
@@ -23,19 +21,12 @@ def main():
 	print_table_entry(table3) 
 	current_sector = struct.unpack_from("<I",table4,8)[0]
 	ebr_start_sector = current_sector
-	# current_sector_size = struct.unpack_from("<I",table,12)[0]
 	parse_EBR_entry()
-
-	print_table_entry()
-
-
 
 def read_sectors(fd, sector,count =  1):
 	fd.seek(sector*512)
-	# 섹터에 512를 곱하는 이유는 섹터의 크기가 512이라서인가
-	# 섹터는 0부터 시작이다.
+	# 섹터에 512를 곱하는 이유는 섹터의 크기가 512이라서인가 그리고  섹터는 0부터 시작이다.
 	tmp_data = fd.read(count*512)
-	# print(tmp_data)
 	return tmp_data
 
 def chk_mbr_sign(tmp_data):
@@ -44,25 +35,17 @@ def chk_mbr_sign(tmp_data):
 		print("이 파티션은 MBR이 아님")
 		return 0
 	return 1
+
 def chk_ebr_end(data_list):
 	for i in data_list:
 		if i != 0x00:
 			return 0
 	return 1
 
-
 def print_table_entry(table):
 	print("================================")
-	# print(table)
-	# print("active :",table[0])
-	# print("CHS1 :",table[1],table[2],table[3])
-	# print("FileSystem :",table[1],table[2],table[3])
-	# print("CHS2 :",table[5],table[6],table[7])
-
 	print("starting LBA :",struct.unpack_from("<I",table,8)[0],"* 512 =",struct.unpack_from("<I",table,8)[0]*512," and HEX is",hex(struct.unpack_from("<I",table,8)[0]*512))
 	print("size :",struct.unpack_from("<I",table,12)[0],"* 512 =",512*struct.unpack_from("<I",table,12)[0])
-
-
 
 def parse_EBR_entry():
 	global current_sector, current_sector_size,f, ebr_start_sector
@@ -77,32 +60,17 @@ def parse_EBR_entry():
 	table4 = partition_dat[48:64] # EBR시작주소 여기서 부터는 다시 짜야한다. ㅎㅎ
 	if chk_ebr_end(table2):
 		print("================================")
-		print(current_sector)
-		print(ebr_start_sector)
-		print(struct.unpack_from("<I",table2,8)[0])
 		print("starting LBA :",current_sector+ struct.unpack_from("<I",table1,8)[0],"* 512 =",(current_sector+ struct.unpack_from("<I",table1,8)[0])*512," and HEX is",hex((current_sector+ struct.unpack_from("<I",table1,8)[0])*512))
 		print("size :",struct.unpack_from("<I",table1,12)[0],"* 512 =",512*struct.unpack_from("<I",table1,12)[0])
 		print("END EBR")
 		exit(0)
 
 	print("================================")
-	print(current_sector)
-	print(ebr_start_sector)
-	print(struct.unpack_from("<I",table2,8)[0])
 	print("starting LBA :",current_sector+ struct.unpack_from("<I",table1,8)[0],"* 512 =",(current_sector+ struct.unpack_from("<I",table1,8)[0])*512," and HEX is",hex((current_sector+ struct.unpack_from("<I",table1,8)[0])*512))
 	print("size :",struct.unpack_from("<I",table1,12)[0],"* 512 =",512*struct.unpack_from("<I",table1,12)[0])
-
 	current_sector = ebr_start_sector + struct.unpack_from("<I",table2,8)[0]
 	print(current_sector)
 	parse_EBR_entry()
-
-	
-
-
-
-
-
-
 
 if __name__ == '__main__':
 	main()
